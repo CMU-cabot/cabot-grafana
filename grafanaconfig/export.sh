835e718ff104bf9c6df7255599c5fe99aa377031
Daisuke Sato <daisukes@cmu.edu>
2024-02-02 01:31:13 -0500

    update import export script
    Signed-off-by: Daisuke Sato <daisukes@cmu.edu>

--- grafanaconfig/export.sh
+++ grafanaconfig/export.sh
@@ -6,4 +6,10 @@ api_key=$(jq -r .key api-key.txt)
 
 echo $api_key
 
-curl -H "Authorization: Bearer $api_key" http://$host/api/datasources -o datasource.json
+curl -H "Authorization: Bearer $api_key" http://$host/api/datasources -o datasources.json
+
+curl -H "Authorization: Bearer $api_key" "http://$host/api/search?query=&type=dash-db" -o dash-db.json
+
+jq -r ".[].uid" dash-db.json | while read -r line; do
+    curl -H "Authorization: Bearer $api_key" http://$host/api/dashboards/uid/$line > dashboard-$line.json
+done
--- grafanaconfig/import.sh
+++ grafanaconfig/import.sh
@@ -6,10 +6,11 @@ api_key=$(jq -r .key api-key.txt)
 
 echo $api_key
 
-jq .[0] datasource.json > temp.json
+jq .[0] datasources.json > temp.json
 
 curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $api_key" \
-     -d @temp.json http://$host/api/datasources
+     -d @temp.json http://$host/api/datasources > /dev/null
 
 
-rm temp.json
+curl -v -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $api_key" \
+     -d @dashboard.json http://$host/api/dashboards/import > /dev/null

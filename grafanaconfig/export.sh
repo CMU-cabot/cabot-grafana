43a3d60453592dece1a8fa38fdd7fe381929306d
Daisuke Sato <daisukes@cmu.edu>
2024-02-03 11:40:21 -0500

    improve export.sh
    Signed-off-by: Daisuke Sato <daisukes@cmu.edu>

--- grafanaconfig/export.sh
+++ grafanaconfig/export.sh
@@ -2,14 +2,60 @@
 
 source common.sh
 
+function help {
+    echo "Usage: $0 <option>"
+    echo ""
+    echo "-h        "
+    echo "-s        export data source"
+    echo "-l        list dashboards"
+    echo "-b <uid>  export specified dashboard"
+}
+
+pwd=$(pwd)
+scriptdir=$(dirname $0)
+cd $scriptdir
+scriptdir=$(pwd)
+
+datasource=0
+list_dashboard=0
+dashboard=
+
+while getopts "hslb:" arg; do
+    case $arg in
+	h)
+	    help
+	    exit
+	    ;;
+	s)
+	    datasource=1
+	    ;;
+	l)
+	    list_dashboard=1
+	    ;;
+	b)
+	    dashboard=$OPTARG
+	    ;;
+    esac
+done
+shift $((OPTIND-1))
+
 api_key=$(jq -r .key api-key.txt)
 
-echo $api_key
 
-curl -H "Authorization: Bearer $api_key" http://$host/api/datasources -o datasources.json
+if [[ $datasource -eq 1 ]]; then
+    curl -H "Authorization: Bearer $api_key" http://$host/api/datasources
+    echo ""
+    echo -en "\033[31m" >&2 # red
+    echo "You need to add your token by adding 'secureJsonData.json'" >&2
+    echo -en "\033[0m" >&2  ## reset color
+fi
 
-curl -H "Authorization: Bearer $api_key" "http://$host/api/search?query=&type=dash-db" -o dash-db.json
 
-jq -r ".[].uid" dash-db.json | while read -r line; do
-    curl -H "Authorization: Bearer $api_key" http://$host/api/dashboards/uid/$line > dashboard-$line.json
-done
+if [[ $list_dashboard -eq 1 ]]; then
+    curl -H "Authorization: Bearer $api_key" "http://$host/api/search?query=&type=dash-db" 2> /dev/null | jq -r '.[] | "\(.title) - \(.uid)"'
+fi
+
+if [[ $dashboard != "" ]]; then
+    curl -H "Authorization: Bearer $api_key" http://$host/api/dashboards/uid/$dashboard | jq 'del(.meta) | .dashboard.id = null'
+fi
+

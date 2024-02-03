d7aec007bc5e2325263013cbb53d2dc87b8d4344
Daisuke Sato <daisukes@cmu.edu>
2024-02-02 23:34:10 -0500

    publish /path plan to influxdb
    Signed-off-by: Daisuke Sato <daisukes@cmu.edu>

--- cabot_influxdb_client/script/client_node.py
+++ cabot_influxdb_client/script/client_node.py
@@ -25,65 +25,88 @@ import rclpy
 from rclpy.node import Node
 from cabot_msgs.msg import PoseLog
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Path
 from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus
 from tf_transformations import euler_from_quaternion
+
+from cabot_ui import geoutil, cabot_rclpy_util
 
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 from datetime import datetime
 import time
 
+class Throttle():
+    def __init__(self, interval):
+        self.interval = interval
+        self.last = time.time()
+
+    def check(self):
+        return time.time() > self.last + self.interval
+
+    def reset(self):
+        self.last = time.time()
+
 
 class ClientNode(Node):
     def __init__(self):
         super().__init__("client_node")
+        cabot_rclpy_util.CaBotRclpyUtil.initialize(self)
 
         self.host = self.declare_parameter("host", "").value
         self.token = self.declare_parameter("token", "").value
         self.org = self.declare_parameter("org", "").value
         self.bucket = self.declare_parameter("bucket", "").value
-
-        self.get_logger().info(f"{[self.host, self.token, self.org, self.bucket]}")
+        anchor_file = self.declare_parameter("anchor_file", "").value
+        self.anchor = None
+        self.get_logger().info(F"Anchor file is {anchor_file}")
+        if anchor_file is not None:
+            temp = geoutil.get_anchor(anchor_file)
+            if temp is not None:
+                self.anchor = temp
+            else:
+                self.get_logger.warn(F"could not load anchor_file \"{anchor_file}\"")
 
         self.client = InfluxDBClient(url=self.host, token=self.token, org=self.org)
         self.write_api = self.client.write_api(write_options=SYNCHRONOUS)
 
         self.pose_log_sub = self.create_subscription(PoseLog, '/cabot/pose_log', self.pose_log_callback, 10)
-        self.last_pose_log = time.time()
-        self.pose_log_interval = 1.0
-        self.anchor_rotate = -128.8
+        self.pose_log_throttle = Throttle(1.0)
 
         self.cmd_vel_sub = self.create_subscription(Twist, '/cabot/cmd_vel', self.cmd_vel_callback, 10)
-        self.last_cmd_vel = time.time()
-        self.cmd_vel_interval = 0.2
+        self.cmd_vel_throttle = Throttle(0.2)
         
         self.diag_agg_sub = self.create_subscription(DiagnosticArray, '/diagnostics_agg', self.diagnostics_callback, 10)
 
-        
+        self.plan_sub = self.create_subscription(Path, '/path', self.path_callback, 10)
+
+        self.current_floor = 0
+
+
     def pose_log_callback(self, msg):
-        if time.time() < self.last_pose_log + self.pose_log_interval:
+        if not self.pose_log_throttle.check():
             return
-        self.get_logger().info(f"{[self.last_pose_log,  self.pose_log_interval,  time.time()]}")
+        self.pose_log_throttle.reset()
         orientation = msg.pose.orientation
         (roll, pitch, yaw) = euler_from_quaternion([orientation.x, orientation.y, orientation.z, orientation.w])
         point = Point("pose_data") \
             .field("lat", msg.lat) \
             .field("lng", msg.lng) \
             .field("floor", msg.floor) \
-            .field("yaw", self.anchor_rotate - yaw/math.pi*180) \
+            .field("yaw", - self.anchor.rotate - yaw/math.pi*180) \
             .time(datetime.utcnow())
+        self.current_floor = msg.floor
         self.write_api.write(bucket=self.bucket, org=self.org, record=point)
-        self.last_pose_log = time.time()
 
     def cmd_vel_callback(self, msg):
-        if time.time() < self.last_cmd_vel + self.cmd_vel_interval:
+        if not self.cmd_vel_throttle.check():
             return
+        self.cmd_vel_throttle.reset()
         v = math.sqrt(msg.linear.x * msg.linear.x + msg.linear.y * msg.linear.y + msg.linear.z * msg.linear.z)
         point = Point("velocity") \
             .field("value", v) \
             .time(datetime.utcnow())
         self.write_api.write(bucket=self.bucket, org=self.org, record=point)
-        self.last_cmd_vel = time.time()
 
     def diagnostics_callback(self, msg):
         target_name = "Soft: Navigation2"
@@ -101,6 +124,21 @@ class ClientNode(Node):
             .field("level", max_level)\
             .time(datetime.utcnow())
         self.write_api.write(bucket=self.bucket, org=self.org, record=point)
+
+    def path_callback(self, msg):
+        for pose in msg.poses:
+            position = pose.pose.position
+            localp = geoutil.Point(x=position.x, y=position.y)
+            globalp = geoutil.local2global(localp, self.anchor)
+            self.get_logger().info(f"{position}")
+            self.get_logger().info(f"{localp} {self.anchor}")
+            self.get_logger().info(f"{globalp}")
+            
+            point = Point("plan") \
+                .field("lat", globalp.lat) \
+                .field("lng", globalp.lng) \
+                .time(datetime.utcnow())
+            self.write_api.write(bucket=self.bucket, org=self.org, record=point)
 
 
 def main(args=None):
